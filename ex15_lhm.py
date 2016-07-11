0d7581c3258025a15416b951ea81c8e44dc9c9ff
iexpo6 <iexpo6@naver.com>
2016-07-12 00:40:28 +0900

    Add files via upload

---
+++ ex15_lhm.py
@@ -0,0 +1,17 @@
+#파이썬을 실행할때 python 파일이름 형식으로 쳐주셔야되요 예를들어 python ex15.py ex13.py
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print("Here's your file {}".format(filename))
+
+print(txt.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
